c3d557a5ed78281c85eb437b81fa27c23bd2c943
mattpettenato <mattpettenato@gmail.com>
2023-01-30 19:31:23 -0800

    can render app.jsx and fixed webpack compile issue

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import App from '../src/components/App'
+import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Router>
-      <Route path="/" component={App}/>
-    </Router>,
+    // <Router>
+    //   <Route path="/" component={App}/>
+    // </Router>,
+    <App/>,
     document.body.appendChild(document.createElement('div')),
   )
 })
