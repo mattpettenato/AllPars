1854bc8b6b6a13168df6e8421322ae979bc5ee13
mattpettenato <mattpettenato@gmail.com>
2023-01-30 17:47:10 -0800

    fixed routes typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  namepace :api do
+  namespace :api do
     namespace :v1 do
       resources :airlines, param: :slug
       resources :reviews, only: [:create, :destroy]
